5cd67007d067a75b5dd4689d8950fd0a2d4dbd71
Shubham Awasthi <aw.shubh@gmail.com>
2018-12-06 15:24:37 +0530

    policy to verify token

---
+++ policies.js
@@ -0,0 +1,20 @@
+var {secret}=require('./rebuild.json')
+
+
+function verify(req,res, next){
+    var hmac = crypto.createHmac("sha1", secret);
+    var calculatedSignature = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
+    console.log(req.headers["x-hub-signature"] === calculatedSignature); // Returns false
+    console.log(req.headers["x-hub-signature"]) // => sha1=blablabla
+    console.log(calculatedSignature) // => sha1=foofoofoo
+    if(req.headers["x-hub-signature"] === calculatedSignature) next();
+    else{
+        res.json({
+            err: 'Unable to verify secret token'
+        })
+    }
+}
+
+module.export={
+    verify
+}
